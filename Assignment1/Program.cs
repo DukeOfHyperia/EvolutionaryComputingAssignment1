0d310f9b09e9c0fe35c2370405d8410969d3703f
Steven Both <steven_both@hotmail.com>
2017-02-21 15:38:50 +0100

    Code base added

---
+++ Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
+    class GeneticAlgorithm
+    {
+
+        public GeneticAlgorithm(int N, int crossoverType, int costFunction)
+        {
+            // Enter here the code for the genetic algorithm
+        }
+
+    }
+
+    class CostFunction
+    {
+        public CostFunction()
+        {
+        }
+    }
+}
